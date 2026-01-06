4d64c2ee8bb772a8fb58afebb762772b79ded9cb
VogeZ-Git <sander.kl.kr@gmail.com>
2026-01-06 13:40:38 +0100

    Add files via upload

--- netlify/functions/save.js
+++ netlify/functions/save.js
@@ -2,32 +2,29 @@ const { Octokit } = require("@octokit/rest");
 
 exports.handler = async (event) => {
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-  const { date, tasks } = JSON.parse(event.body);
+  const { tasks } = JSON.parse(event.body);
+  const date = new Date().toISOString().split('T')[0];
   const path = `days/${date}.json`;
 
   try {
-    // Try to get the file if it exists to get its 'sha' (ID)
     let sha;
     try {
       const { data } = await octokit.repos.getContent({
-        owner: 'Vogez-Git',
+        owner: 'Vogez-Git', // CHANGE THIS
         repo: 'Repo1',
         path
       });
       sha = data.sha;
-    } catch (e) { /* File doesn't exist yet, that's fine */ }
+    } catch (e) {}
 
     await octokit.repos.createOrUpdateFileContents({
-      owner: 'Vogez-Git',
+      owner: 'Vogez-Git', // CHANGE THIS
       repo: 'Repo1',
       path,
-      message: `Update routine for ${date}`,
+      message: `Update ${date}`,
       content: Buffer.from(JSON.stringify(tasks)).toString('base64'),
       sha
     });
-
-    return { statusCode: 200, body: "Saved!" };
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() };
-  }
+    return { statusCode: 200, body: "OK" };
+  } catch (err) { return { statusCode: 500, body: err.toString() }; }
 };
